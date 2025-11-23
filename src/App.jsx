98c3c7f06f849ba1c59907a5c035f9b86c44c252
Alexander Pereira <xandi9731@gmail.com>
2025-11-23 17:08:33 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { CorProvider } from "./context/CorContext";
-import Main from "./Pages/main";
+import Main from "./pages/main";
 
 function App() {
   return (
